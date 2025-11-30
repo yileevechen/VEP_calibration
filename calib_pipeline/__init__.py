aa4da3935d18d270bc205a14e924cf3757f3c2fc
Yile Chen <yilechen@uw.edu>
2025-11-30 14:48:04 -0800

    Create __init__.py

---
+++ calib_pipeline/__init__.py
@@ -0,0 +1,8 @@
+"""Calibration package.
+
+This package contains:
+- prepare_data: build labeled/unlabeled data per gene
+- get_best_fit: fit distributions to PLP/BLB scores and choose a mixture method
+- data_generation: generate simulated calibration/test/unlabeled data
+- sim_data_step00: orchestration for the whole step-00 pipeline
+"""
