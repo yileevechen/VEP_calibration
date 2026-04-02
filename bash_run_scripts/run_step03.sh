4c1199118a1ca45bac6647fd1434adeef5757f2b
Yile Chen <yilechen@uw.edu>
2026-04-02 01:59:21 -0700

    Update run_step03.sh

--- bash_run_scripts/run_step03.sh
+++ bash_run_scripts/run_step03.sh
@@ -3,12 +3,14 @@
 #BSUB -q premium
 #BSUB -n 16
 #BSUB -W 08:00
-#BSUB -J calib_step03[1]
-#BSUB -e "logs/calib_step03.%J.%I.err"
-#BSUB -o "logs/calib_step03.%J.%I.out"
-
-ARRAY_IDX=${LSB_JOBINDEX}
+#BSUB -J calib_step03
+#BSUB -e "logs/calib_step03.%J.err"
+#BSUB -o "logs/calib_step03.%J.out"
 
 source activate calibrate_para
 
-python -m calib_pipeline.calib_step03 "$ARRAY_IDX"
+python -m calib_pipeline.calib_step03 \
+    --gene BRCA1 \
+    --predictor REVEL \
+    --prior 0.12 \
+    --outdir /path/to/output
