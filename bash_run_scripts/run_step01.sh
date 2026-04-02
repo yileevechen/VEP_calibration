f42d2abb9fd3aecef716674b3eb437d21ba05b84
Yile Chen <yilechen@uw.edu>
2026-04-02 01:24:58 -0700

    Update run_step01.sh

--- bash_run_scripts/run_step01.sh
+++ bash_run_scripts/run_step01.sh
@@ -3,12 +3,18 @@
 #BSUB -q premium
 #BSUB -n 10
 #BSUB -W 06:00
-#BSUB -J calib_step01[1]
+#BSUB -J calib_step01[1-30]
 #BSUB -e "logs/calib_step01.%J.%I.err"
 #BSUB -o "logs/calib_step01.%J.%I.out"
 
-ARRAY_IDX=${LSB_JOBINDEX}
+# use LSF array index as seed
+SEED=${LSB_JOBINDEX}
 
 source activate calibrate_para
 
-python -m calib_pipeline.calib_step01 "$ARRAY_IDX"
+python -m calib_pipeline.calib_step01 \
+    --gene BRCA1 \
+    --predictor REVEL \
+    --alpha 0.12 \
+    --base_dir /path/to/output \
+    --seed ${SEED}
